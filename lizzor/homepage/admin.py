a4661107af24c077488eafdd86959f8c238e23d4
Eugene <tatsuke.private@gmail.com>
2023-04-26 02:51:11 +0300

    Registered models at  admin panel

--- lizzor/homepage/admin.py
+++ lizzor/homepage/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(Article)
+admin.site.register(Subcategory)
+admin.site.register(Category)
+admin.site.register(Image)
+admin.site.register(Author)
+
