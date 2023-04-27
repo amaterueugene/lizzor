85a5de43ffdf9f8e06c68446d653b27ca883fb4a
Eugene <tatsuke.private@gmail.com>
2023-04-27 23:48:56 +0300

    Created header

--- lizzor/homepage/utils.py
+++ lizzor/homepage/utils.py
@@ -5,5 +5,5 @@ from .models import *
 class DataMixin:
     def get_user_context(self, **kwargs):
         context = kwargs
-        
+        context['categories'] = Category.objects.all()
         return context
