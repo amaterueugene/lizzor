f59d8a5ee51dea2bfd15cb3a2f6fcd1bbd6288de
Eugene <tatsuke.private@gmail.com>
2023-05-02 19:42:20 +0300

    Added articles filtering with categories slugs

--- lizzor/homepage/urls.py
+++ lizzor/homepage/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from django.conf.urls.static import static
-from .views import ShowArticlesView
+from .views import ShowArticlesView, ShowCatArticlesView
 from lizzor import settings
 
 
 urlpatterns = [
     path('', ShowArticlesView.as_view(), name='HomePage'),
+    path('category/<slug:category_slug>', ShowCatArticlesView.as_view(), name='CategoryPage'),
 ]
 
 if settings.DEBUG:
--- lizzor/homepage/views.py
+++ lizzor/homepage/views.py
@@ -15,9 +15,32 @@ class ShowArticlesView(DataMixin, ListView):
         context = super().get_context_data(**kwargs)
         # Два поста для слайдера. Вне категорий.
         context['slider_posts'] = Article.objects.all().order_by('-pk')[:2]
+
         # Выбранная категория. По дефолту - бизнес
-        if 'cat_selected' not in context:
-            context['cat_selected'] = Category.objects.get(slug='business')
+        context['cat_selected'] = Category.objects.get(slug='business')
         
         c_def = self.get_user_context(title='Главная страница', cat_selected=context['cat_selected'])
         return dict(list(context.items())+list(c_def.items()))
+    
+
+class ShowCatArticlesView(DataMixin, ListView):
+    model = Article
+    template_name = 'homepage/index.html'
+    context_object_name = 'articles'
+
+    paginate_by = 10
+
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Два поста для слайдера. Вне категорий.
+        context['slider_posts'] = Article.objects.all().order_by('-pk')[:2]
+        # Выбранная категория. По дефолту - бизнес
+        context['cat_selected'] = Category.objects.get(slug=self.kwargs['category_slug'])
+        
+        c_def = self.get_user_context(title='Главная страница', cat_selected=context['cat_selected'])
+        return dict(list(context.items())+list(c_def.items()))
+    
+
+    def get_queryset(self):
+        return Article.objects.filter(category__slug=self.kwargs['category_slug'])
